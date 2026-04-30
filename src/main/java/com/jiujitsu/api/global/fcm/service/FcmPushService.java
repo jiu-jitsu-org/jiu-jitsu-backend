4014fab58386ea31fa11d64c802806a8d3a1cf58
김세현 <ssshyn.dev@gmail.com>
2026-04-30 15:01:49 +0900

    # [BE] feat: fcm 토큰 테스트_댓글작성 알림추가 Refs #37

--- src/main/java/com/jiujitsu/api/global/fcm/service/FcmPushService.java
+++ src/main/java/com/jiujitsu/api/global/fcm/service/FcmPushService.java
@@ -1,10 +1,13 @@
 package com.jiujitsu.api.global.fcm.service;
 
+import com.jiujitsu.api.domain.user.entity.User;
+import com.jiujitsu.api.domain.user.entity.UserAppInfo;
 import com.jiujitsu.api.global.fcm.client.FcmClient;
 import com.jiujitsu.api.global.fcm.entity.FcmPushType;
 import com.jiujitsu.api.global.fcm.dto.FcmMessage;
 import com.jiujitsu.api.global.fcm.dto.FcmRequest;
 import com.jiujitsu.api.global.fcm.dto.FcmNotification;
+import com.jiujitsu.api.global.fcm.entity.PushSendLog;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,26 +16,50 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FcmPushService {
     private final FcmClient fcmClient;
+    private final PushSendLogService pushSendLogService;
 
     @Value("${fcm.project-id}")
     private String projectId;
 
-    public void send(String token, FcmPushType pushType) {
-        FcmRequest request =
-                FcmRequest.builder()
-                        .message(
-                                FcmMessage.builder()
-                                        .token(token)
-                                        .fcmNotification(
-                                                FcmNotification.builder()
-                                                        .title(pushType.getTitle())
-                                                        .body(pushType.getBody())
-                                                        .build()
-                                        )
-                                        .build()
-                        )
-                        .build();
+    public void send(User user, FcmPushType pushType) {
+        boolean isSuccess = true;
 
-        fcmClient.sendPush(projectId, request);
+        for (UserAppInfo appInfo : user.getAppInfos()) {
+            try {
+                FcmRequest request =
+                        FcmRequest.builder()
+                                .message(
+                                        FcmMessage.builder()
+                                                .token(appInfo.getToken())
+                                                .fcmNotification(
+                                                        FcmNotification.builder()
+                                                                .title(pushType.getTitle())
+                                                                .body(pushType.getBody())
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build();
+
+                fcmClient.sendPush(projectId, request);
+            } catch (Exception e) {
+                isSuccess = false;
+            }
+
+            // log
+            PushSendLog pushSendLog = PushSendLog.builder()
+                    .isSuccess(isSuccess)
+                    .errorCode("0001")
+                    .errorMessage("message")
+                    .pushType(pushType)
+                    .title(pushType.getTitle())
+                    .body(pushType.getBody())
+                    .userAppInfo(appInfo)
+                    .build();
+
+            pushSendLogService.insertPushSendLog(pushSendLog);
+        }
+
+
     }
 }
