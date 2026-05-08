7aa5d4dfe4d8eb59e7a17c86248329c71d8b72e5
KiU <41310752+kyuchanE@users.noreply.github.com>
2026-05-08 22:09:12 +0900

    # [BE] feat: FcmTokenProvider

--- src/main/java/com/jiujitsu/api/global/fcm/provider/FcmTokenProvider.java
+++ src/main/java/com/jiujitsu/api/global/fcm/provider/FcmTokenProvider.java
@@ -3,7 +3,6 @@ package com.jiujitsu.api.global.fcm.provider;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.springframework.stereotype.Component;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -12,9 +11,7 @@ public class FcmTokenProvider {
     public String getAccessToken() throws IOException {
 
         GoogleCredentials googleCredentials =
-                GoogleCredentials.fromStream(
-                        new FileInputStream("src/main/resources/fcm/firebase-key.json")
-                ).createScoped(
+                GoogleCredentials.getApplicationDefault().createScoped(
                         List.of("https://www.googleapis.com/auth/firebase.messaging")
                 );
 
